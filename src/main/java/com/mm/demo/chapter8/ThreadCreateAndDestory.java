f55633ce781922ea293fba35aca1af05a41a0270
supermenG <852458550@qq.com>
2019-05-23 17:45:37 +0800

    线程池的创建销毁和饱和策略

---
+++ src/main/java/com/mm/demo/chapter8/ThreadCreateAndDestory.java
@@ -0,0 +1,94 @@
+package com.mm.demo.chapter8;
+
+import java.util.concurrent.*;
+
+/**
+ * 线程池的创建和销毁，已经各个配置的修改
+ * 测试结果：
+ * 有界线程池，有界队列，当线程满后不会再创建新的线程。如果队列满了，则任务被抛弃。（默认饱和策略为absortPolicy）
+ *
+ */
+public class ThreadCreateAndDestory {
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor executorService = new ThreadPoolExecutor(2,
+                3,5, TimeUnit.SECONDS,
+               new ArrayBlockingQueue<Runnable>(2));
+
+        //设置饱和策略
+//        executorService.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
+        //测试线程池中 的线程数等于线程池的基本大小时，仅当工作队列满的时候才会创建线程
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(6000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("任务1线程"+Thread.currentThread().getName());
+            }
+        });
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(6000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("任务2线程"+Thread.currentThread().getName());
+            }
+        });
+        /*
+        两个之后线程不会再创建。只有两个线程执行任务。
+        测试结果：执行第三个任务的线程是第一个任务的线程。
+         */
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("任务3线程" + Thread.currentThread().getName());
+            }
+        });
+/*
+        两个之后线程不会再创建。只有两个线程执行任务。
+        测试结果：执行第三个任务的线程是第一个任务的线程。
+         */
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("任务4线程" + Thread.currentThread().getName());
+            }
+        });
+        /*
+        两个之后线程不会再创建。只有两个线程执行任务。
+        测试结果：执行第三个任务的线程是第一个任务的线程。
+         */
+        try {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("任务5线程" + Thread.currentThread().getName());
+                }
+            });
+        } catch (RejectedExecutionException e) {
+            System.out.println("任务5被抛弃");
+        }
+        /*
+        两个之后线程不会再创建。只有两个线程执行任务。
+        测试结果：执行第三个任务的线程是第一个任务的线程。
+         */
+        try {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("任务6线程" + Thread.currentThread().getName());
+                }
+            });
+        } catch (RejectedExecutionException e) {
+            System.out.println("任务6被抛弃");
+        }
+        executorService.shutdown();
+    }
+}
--- src/main/java/com/mm/queue/QueueDemo.java
+++ src/main/java/com/mm/queue/QueueDemo.java
@@ -7,17 +7,25 @@ import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 队列使用
+ * 公平和非公平锁的使用
  */
 public class QueueDemo {
     /**
      * 公平锁能保证，先put的肯定会先放到队列。
      * 非公平锁不能保证。
+     * 队列使用公平锁，那么队列是FIFO的
      */
-    ArrayBlockingQueue queue = new ArrayBlockingQueue(2, false);//默认使用非公平锁。不能先进先出
+
+    /**
+     * 测试1：使用 了公平锁或非公平锁的队列ArrayLinkQueue
+     * ArrayLinkQueue：达到容量，就等待
+     */
+    ArrayBlockingQueue arrayQueue = new ArrayBlockingQueue(2, false);//默认使用非公平锁。不能先进先出
     CyclicBarrier cyclicBarrier = new CyclicBarrier(10);
 
     public class Productor extends Thread {
@@ -37,7 +45,7 @@ public class QueueDemo {
             }
             try {
                 System.out.println("开始-" + val);
-                queue.put(val);
+                arrayQueue.put(val);
                 System.out.println("结束 -"+val);
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -46,7 +54,7 @@ public class QueueDemo {
     }
 
     /**
-     * 测试ArrayLinkQueue使用公平和非公平锁的区别
+     * ArrayLinkQueue使用公平和非公平锁的区别
      * @throws InterruptedException
      */
     @Test
@@ -62,13 +70,16 @@ public class QueueDemo {
         //输出
         Thread.sleep(4000);
         for (int i = 0; i < 10; i++) {
-            System.out.println("输出的顺序"+queue.take());
+            System.out.println("输出的顺序"+ arrayQueue.take());
         }
 
         Thread.sleep(4000);
 
     }
 
+    /**
+     * 测试2：公平锁或非公平锁的演示
+     */
     final ReentrantLock reentrantLock = new ReentrantLock(true);
     public class LockTest extends Thread {
         private Integer val;
@@ -98,6 +109,58 @@ public class QueueDemo {
             list.get(i).start();
         }
         Thread.sleep(3000);
+    }
+
+    /**
+     * 测试3：队列 LinkedBlockingQueue
+     */
+    LinkedBlockingQueue linkedBlockingQueue = new LinkedBlockingQueue(2);
+    public class Productor2 extends Thread {
+        private Integer val;
+
+        public Productor2(int val) {
+            this.val = val;
+        }
+
+        public void run() {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            try {
+                System.out.println("开始-" + val);
+                linkedBlockingQueue.put(val);
+                System.out.println("结束 -"+val);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * LinkedBlockingQueue使用公平和非公平锁的区别
+     * @throws InterruptedException
+     */
+    @Test
+    public void testLinkBlockQueu() throws InterruptedException {
+        List<Productor2> list = new ArrayList<Productor2>();
+        for (int i = 0; i < 10; i++) {
+            list.add(new Productor2(i));
+        }
+        for (int i = 0; i < 10; i++) {
+            list.get(i).start();
+        }
+        //输出
+        Thread.sleep(3000);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("输出的顺序"+ linkedBlockingQueue.take());
+        }
+
+        Thread.sleep(4000);
+
     }
 
 }
